e2d13e22311a08e26d05426813c7269286ef32d9
Drexter-07 <59890906+Drexter-07@users.noreply.github.com>
2024-10-25 16:47:27 +0530

    Time: 6 ms (68.98%), Space: 8.6 MB (36.90%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+          long int i=x; 
+        long int n=0;
+            while(i>0)
+            {
+                n=i%10+n*10;
+                i/=10;
+            }
+    return n==x;    
+        
+    }
+};
